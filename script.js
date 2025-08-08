a135dc26c1d41be880d5c0441faa320b83c500ce
BLACKINKCOFFEE <160691932+BLACKINKCOFFEE@users.noreply.github.com>
2025-08-08 11:24:43 -0400

    Build marketing site with conversion-focused layout

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Sample website loaded.');
+document.getElementById('newsletter-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thanks for subscribing!');
+  this.reset();
+});
+
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    if (this.hash) {
+      e.preventDefault();
+      document.querySelector(this.hash).scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
